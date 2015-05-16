e581581ce52e31e6a7cd35aaa7194a0c2896c210
xblia <worldandyou@163.com>
2015-05-16 21:50:04 +0800

    Add project first time.

---
+++ ZDingzhicun/src/com/xnet/DBUtil.java
@@ -0,0 +1,12 @@
+package com.xnet;
+
+/**
+ * @author xblia
+ */
+public class DBUtil
+{
+	public static void main(String[] args)
+    {
+	    System.out.println("Hello hibernate world.");
+    }
+}
